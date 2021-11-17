2473c362996552fc815b0348270aee14dbacc07f
Artem Motorev <artem-m-14@mail.ru>
2021-11-17 21:16:51 +0700

    Last

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -15,6 +15,44 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
+        public void Variant1()
+        {
+            var path = "C:\\Users\\artem\\Desktop\\test.txt";
+
+            // Запускаем и находим окно приложения
+            var notepad = Application.Launch("notepad");
+            var winMain = notepad.GetWindow("Untitled - Notepad");
+
+            // Находим обласить ввода и вводим текст
+            var textArea = winMain.Get<TextBox>(SearchCriteria.ByAutomationId("15"));
+            textArea.Text = "Any text with a length of 10-100 characters.";
+
+            // Открываем меню и сохранем
+            var menu = winMain.Get<MenuBar>(SearchCriteria.ByAutomationId("MenuBar"));
+            var btnSaveAs = menu.MenuItem("File", "Save As...");
+            btnSaveAs.Click();
+
+            // Сохраняем указываем указываем путь
+            var winSaveFileDialog = winMain.ModalWindow("Save As");
+            var tbFileName = winSaveFileDialog.MdiChild(SearchCriteria.ByAutomationId("1001"));
+            tbFileName.Enter(path);
+            var btnSave = winSaveFileDialog.Get<Button>(SearchCriteria.ByText("Save"));
+            btnSave.Click();
+
+            // В случае перезаписи файла
+            var winConfirmationDialog = winSaveFileDialog.ModalWindow("Confirm Save As");
+            if (winConfirmationDialog != null)
+            {
+                var btnYes = winConfirmationDialog.Get<Button>(SearchCriteria.ByText("Yes"));
+                btnYes.Click();
+            }
+
+            // Закрываем
+            notepad.Close();
+
+        }
+
+        [TestMethod]
         public void Case1()
         {
             // Запускаем и находим окно приложения
@@ -80,44 +118,6 @@ namespace UnitTestProject1
 
             // Проверка
             Assert.AreEqual(response, "0");
-
-        }
-
-        [TestMethod]
-        public void Variant1()
-        {
-            var path = "C:\\Users\\artem\\Desktop\\test.txt";
-
-            // Запускаем и находим окно приложения
-            var notepad = Application.Launch("notepad");
-            var winMain = notepad.GetWindow("Untitled - Notepad");
-
-            // Находим обласить ввода и вводим текст
-            var textArea = winMain.Get<TextBox>(SearchCriteria.ByAutomationId("15"));
-            textArea.Text = "Any text with a length of 10-100 characters.";
-
-            // Открываем меню и сохранем
-            var menu = winMain.Get<MenuBar>(SearchCriteria.ByAutomationId("MenuBar"));
-            var btnSaveAs = menu.MenuItem("File", "Save As...");
-            btnSaveAs.Click();
-
-            // Сохраняем указываем указываем путь
-            var winSaveFileDialog = winMain.ModalWindow("Save As");
-            var tbFileName = winSaveFileDialog.MdiChild(SearchCriteria.ByAutomationId("1001"));
-            tbFileName.Enter(path);
-            var btnSave = winSaveFileDialog.Get<Button>(SearchCriteria.ByText("Save"));
-            btnSave.Click();
-
-            // В случае перезаписи файла
-            var winConfirmationDialog = winSaveFileDialog.ModalWindow("Confirm Save As");
-            if (winConfirmationDialog != null)
-            {
-                var btnYes = winConfirmationDialog.Get<Button>(SearchCriteria.ByText("Yes"));
-                btnYes.Click();
-            }
-
-            // Закрываем
-            notepad.Close();
 
         }
     }
