79143d02e9df9b447f7aa4e9ea14d2a7ff4d69ef
Artem Motorev <artem-m-14@mail.ru>
2021-11-17 20:24:13 +0700

    Add two case for test calculator

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
+using System.Threading;
 using TestStack.White;
 using TestStack.White.UIItems;
 using TestStack.White.UIItems.Finders;
@@ -19,24 +20,74 @@ namespace UnitTestProject1
             // Запускаем и находим окно приложения
             var calc = Application.Launch("calc");
             var winMain = calc.GetWindow("Calculator");
-            Button button = winMain.Get<Button>(SearchCriteria.ByText("6"));
-            button.Click();
-            button = winMain.Get<Button>(SearchCriteria.ByText("Add"));
-            button.Click();
-            button = winMain.Get<Button>(SearchCriteria.ByText("4"));
-            button.Click();
-            button = winMain.Get<Button>(SearchCriteria.ByText("Equals"));
-            button.Click();
+
+            // Нажатие на кнопку "1" 4 раза
+            var button = winMain.Get<Button>(SearchCriteria.ByText("1"));
+            button.Click(); button.Click(); button.Click(); button.Click();
+
+            // Нажатие на кнопку "*"
+            var button1 = winMain.Get<Button>(SearchCriteria.ByText("Multiply"));
+            button1.Click();
+
+            // Нажатие на кнопку "1" 4 раза
+            button.Click(); button.Click(); button.Click(); button.Click();
+
+            // Нажатие на кнопку "="
+            var button3 = winMain.Get<Button>(SearchCriteria.ByText("Equals"));
+            button3.Click();
+
+            // Получение ответа и преобразование
+            var res = winMain.Get<Label>(SearchCriteria.ByAutomationId("150"));
+            var response = res.Name.ToString();
+
+            // Закрытие калькулятора
             calc.Close();
 
-            //Assert.IsTrue(File.Exists(path));
-            //Assert.AreEqual(File.ReadAllText(path), "Any text with a length of 10-100 characters.");
+            // Проверка
+            Assert.AreEqual(response, "1234321");
+
         }
 
-    [TestMethod]
+        [TestMethod]
+        public void Case2()
+        {
+            // Запускаем и находим окно приложения
+            var calc = Application.Launch("calc");
+            var winMain = calc.GetWindow("Calculator");
+
+            // Нажатие на кнопку "2" 4 раза
+            var button = winMain.Get<Button>(SearchCriteria.ByText("2"));
+            button.Click(); button.Click(); button.Click(); button.Click();
+
+            // Нажатие на кнопку "*"
+            var button1 = winMain.Get<Button>(SearchCriteria.ByText("Subtract"));
+            button1.Click();
+
+            // Нажатие на кнопку "1" 4 раза
+            var button2 = winMain.Get<Button>(SearchCriteria.ByText("1"));
+            button2.Click(); button2.Click(); button2.Click(); button2.Click();
+
+            // Нажатие на кнопку "="
+            var button3 = winMain.Get<Button>(SearchCriteria.ByText("Equals"));
+            button3.Click();
+
+            // Получение ответа и преобразование
+            var res = winMain.Get<Label>(SearchCriteria.ByAutomationId("150"));
+            var response = res.Name.ToString();
+
+            // Закрытие калькулятора
+            calc.Close();
+
+            // Проверка
+            Assert.AreEqual(response, "0");
+
+        }
+
+        [TestMethod]
         public void Variant1()
         {
             var path = "C:\\Users\\artem\\Desktop\\test.txt";
+
             // Запускаем и находим окно приложения
             var notepad = Application.Launch("notepad");
             var winMain = notepad.GetWindow("Untitled - Notepad");
@@ -56,20 +107,18 @@ namespace UnitTestProject1
             tbFileName.Enter(path);
             var btnSave = winSaveFileDialog.Get<Button>(SearchCriteria.ByText("Save"));
             btnSave.Click();
+
             // В случае перезаписи файла
-            /*
             var winConfirmationDialog = winSaveFileDialog.ModalWindow("Confirm Save As");
             if (winConfirmationDialog != null)
             {
                 var btnYes = winConfirmationDialog.Get<Button>(SearchCriteria.ByText("Yes"));
                 btnYes.Click();
             }
-            */
-            // Закрываем и проверяем
+
+            // Закрываем
             notepad.Close();
 
-            //Assert.IsTrue(File.Exists(path));
-            //Assert.AreEqual(File.ReadAllText(path), "Any text with a length of 10-100 characters.");
         }
     }
 }
